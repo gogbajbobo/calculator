341c7fca1c234549148062dff50ecd6cca2641c7
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2012-04-17 18:26:26 +0400

    add button Graph with segue to GraphViewController

---
+++ RPN Calculator/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.h
+//  RPN Calculator
+//
+//  Created by Григорьев Максим on 4/17/12.
+//  Copyright (c) 2012 IMT RAS, ИПТМ РАН. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController
+
+@end
---
+++ RPN Calculator/GraphViewController.m
@@ -0,0 +1,58 @@
+//
+//  GraphViewController.m
+//  RPN Calculator
+//
+//  Created by Григорьев Максим on 4/17/12.
+//  Copyright (c) 2012 IMT RAS, ИПТМ РАН. All rights reserved.
+//
+
+#import "GraphViewController.h"
+
+@interface GraphViewController ()
+
+@end
+
+@implementation GraphViewController
+
+
+
+
+
+
+
+
+
+
+
+
+// ________________________________________________________________________
+
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
