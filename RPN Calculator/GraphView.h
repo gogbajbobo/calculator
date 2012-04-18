648aa834633937e0de0784b923c41292c13e5a4f
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2012-04-18 15:09:05 +0400

    create UIView GraphView

---
+++ RPN Calculator/GraphView.h
@@ -0,0 +1,13 @@
+//
+//  GraphView.h
+//  RPN Calculator
+//
+//  Created by Григорьев Максим on 4/18/12.
+//  Copyright (c) 2012 IMT RAS, ИПТМ РАН. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphView : UIView
+
+@end
---
+++ RPN Calculator/GraphView.m
@@ -0,0 +1,41 @@
+//
+//  GraphView.m
+//  RPN Calculator
+//
+//  Created by Григорьев Максим on 4/18/12.
+//  Copyright (c) 2012 IMT RAS, ИПТМ РАН. All rights reserved.
+//
+
+#import "GraphView.h"
+
+@implementation GraphView
+
+- (void)setup
+{
+    self.contentMode = UIViewContentModeRedraw;
+}
+
+- (void)awakeFromNib
+{
+    [self setup];
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
