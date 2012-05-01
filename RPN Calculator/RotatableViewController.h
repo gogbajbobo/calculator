2cb7d9deafecabfe74ae2c8e4466eb4d00565989
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2012-05-01 23:11:26 +0400

    add RotatableVC and SplitView…Presenter from Psychologist project

---
+++ RPN Calculator/RotatableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RotatableViewController.h
+//  Psychologist
+//
+//  Created by Григорьев Максим on 4/29/12.
+//  Copyright (c) 2012 Maxim V. Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotatableViewController : UIViewController <UISplitViewControllerDelegate>
+
+@end
---
+++ RPN Calculator/SplitViewBarButtonItemPresenter.h
@@ -0,0 +1,15 @@
+//
+//  SplitViewBarButtonItemPresenter.h
+//  Psychologist
+//
+//  Created by Григорьев Максим on 4/29/12.
+//  Copyright (c) 2012 Maxim V. Grigoriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol SplitViewBarButtonItemPresenter <NSObject>
+
+@property (nonatomic, strong) UIBarButtonItem *splitViewBarButtonItem;
+
+@end
